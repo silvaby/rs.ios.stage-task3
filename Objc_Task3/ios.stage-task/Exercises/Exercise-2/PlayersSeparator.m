0002057c425743519ecf6699ceff80e405dbcb5b
Dzmitry <skysilvaby@gmail.com>
2021-05-23 23:37:40 +0300

    feat: exercise-2 done

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,22 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSInteger count = 0;
+    
+    for (NSInteger i = 0; i < ratingArray.count; ++i) {
+        for (NSInteger j = i + 1; j < ratingArray.count; ++j) {
+            for (NSInteger k = j + 1; k < ratingArray.count; ++k) {
+            if (((([ratingArray[i] intValue] < [ratingArray[j] intValue]) &&
+                  ([ratingArray[j] intValue]) < [ratingArray[k] intValue])) ||
+                ((([ratingArray[i] intValue] > [ratingArray[j] intValue]) &&
+                  ([ratingArray[j] intValue] > [ratingArray[k] intValue])))) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
 }
 
 @end
