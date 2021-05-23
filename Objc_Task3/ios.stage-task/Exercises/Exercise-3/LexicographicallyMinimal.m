89c5142c3354b4681fa6017f9652ac2a10261bd7
Dzmitry <skysilvaby@gmail.com>
2021-05-23 23:47:54 +0300

    feat: exercise-3 done

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,26 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSString *stringOne = [[NSString alloc] initWithString: string1];
+    NSString *stringTwo = [[NSString alloc] initWithString: string2];
+    NSMutableString *result = [[NSMutableString alloc] init];
+
+    while ([stringOne length] && [stringTwo length]) {
+        const char *one = [stringOne UTF8String];
+        const char *two = [stringTwo UTF8String];
+        
+        if (one[0] < two[0] || one[0] == two[0]) {
+            [result appendString: [stringOne substringToIndex: 1]];
+            stringOne = [stringOne substringFromIndex: 1];
+        } else {
+            [result appendString: [stringTwo substringToIndex: 1]];
+            stringTwo = [stringTwo substringFromIndex: 1];
+        }
+    }
+    
+    [stringOne length] ? [result appendString: stringOne] : [result appendString: stringTwo];
+    
+    return result;
 }
 
 @end
